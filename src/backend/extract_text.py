f083a70905f796c7800d6dab7380fe20f3545d69
Andrzej-Tezyk <andrewtezhik@gmail.com>
2025-04-11 12:44:28 +0200

    remove data cleaning function

--- src/backend/extract_text.py
+++ src/backend/extract_text.py
@@ -9,37 +9,6 @@ import pdfplumber
 log = logging.getLogger("__name__")
 
 
-POLISH_CHARS = "ąćęłńóśźżĄĆĘŁŃÓŚŹŻ"
-
-
-def clean_polish_spacing(text: str) -> str:
-    """
-    A problem with extracting text from .pdf in polish language was found. 
-    When polish alphabet letter is within a word a " " is created before it.
-    Random unnecessary spaces are created as a result of how pypdf2 works.
-    PyPDF2 reconstruct text based on proximity, which isn't always accurate.
-    Polish characters (or any accented glyphs) might be slightly offset in the font, 
-    causing spacing algorithms to think they’re a new "word" and insert a space.
-    Like: "słówko" -> "s ł ó wko"
-
-    Args:
-        text: Text extracted from pdf document.
-
-    Returns:
-        A string containing cleaned text.
-    """
-
-    # remove line breakes that are not paragraph breakes
-    text = text.replace('\r\n', '\n').replace('\r', '\n')
-
-    # remove spaces before Polish chars
-    text = re.sub(rf"\s([{POLISH_CHARS}])", r"\1", text)
-
-    # fix hypernated line breakes
-    text = re.sub(r"-\s*\n\s*", "", text)
-
-    return text.strip()
-
 def extract_text_from_pdf(pdf_path: Path) -> str:
     """Extracts text from a PDF file.
 

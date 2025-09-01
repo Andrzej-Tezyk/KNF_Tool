06786bac0b5c238e3d00277ff53c82b3834d73b0
Andrzej-Tezyk <andrewtezhik@gmail.com>
2025-09-01 02:19:11 +0200

    finished docker dev

--- src/backend/rag/chroma_instance.py
+++ src/backend/rag/chroma_instance.py
@@ -1,9 +1,12 @@
 import logging
+import os
 from typing import Any
 
 import chromadb
 
 log = logging.getLogger("__name__")
+
+os.environ["CHROMA_TELEMETRY_ENABLED"] = "false"
 
 
 def get_chroma_client(path: str) -> Any:
@@ -15,4 +18,7 @@ def get_chroma_client(path: str) -> Any:
     Returns:
         chromadb.PersistentClient: An instance of the ChromaDB client.
     """
-    return chromadb.PersistentClient(path=path)
+    settings = chromadb.config.Settings()
+    settings.anonymized_telemetry = False
+
+    return chromadb.PersistentClient(path=path, settings=settings)
